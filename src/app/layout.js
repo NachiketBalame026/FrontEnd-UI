fa532283e21bfdb52acccf4dc0732a8045192927
Nachiket Balame <nachiketbalame001@gmail.com>
2024-12-10 06:42:05 +0530

    Final

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,6 +12,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
+
       <body className="bg-gray-50">
         <Navbar />
         <main className="p-6">{children}</main>
